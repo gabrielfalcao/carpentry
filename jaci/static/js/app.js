52cc2d23346a1afe1556275fcbe4edba364df21e
Gabriel Falcão <gabriel@nacaolivre.org>
2015-05-30 06:11:51 -0400

    indentation

--- jaci/static/js/app.js
+++ jaci/static/js/app.js
@@ -51,81 +51,81 @@ angular.module("JaciApp", [
 
 }).run(function($rootScope, $state, $templateCache, $http, notify, hotkeys){
 })
-.directive('navbar', function($rootScope, $state, $location) {
-    return {
-        restrict: 'E',
-        templateUrl: "/assets/js/templates/navbar.html",
-        link: function (scope, element, attrs) {
-        }
-    }
-})
-    .controller("JaciMainCtrl", function($scope, $http, $location, $rootScope, hotkeys, $state, $templateCache){
-    $rootScope.go = function ( path ) {
-        $location.path( path );
-    };
-
-            hotkeys.add({
-        combo: 'N',
-        description: 'create a new builder',
-        callback: function(){
-            $rootScope.go("/new-builder");
-        }
-    });
-    hotkeys.add({
-        combo: 'F',
-        description: 'fullscreen',
-        callback: function(){
-            $rootScope.go("/fullscreen");
-        }
-    });
-    hotkeys.add({
-        combo: 'P',
-        description: 'preferences',
-        callback: function(){
-            $rootScope.go("/preferences");
-        }
-    });
-    hotkeys.add({
-        combo: 'D',
-        description: 'dashboard',
-        callback: function(){
-            $rootScope.go("/");
-        }
-    });
-
-    hotkeys.add({
-        combo: 'esc',
-        description: 'previous screen',
-        callback: function(){
-            if ($rootScope.stateStack.length > 0) {
-                var name = $rootScope.stateStack.pop();
-                $rootScope.go(name);
-                $rootScope.stateStack.pop();
-
-            } else {
-                $rootScope.go("index");
+    .directive('navbar', function($rootScope, $state, $location) {
+        return {
+            restrict: 'E',
+            templateUrl: "/assets/js/templates/navbar.html",
+            link: function (scope, element, attrs) {
             }
+        }
+    })
+    .controller("JaciMainCtrl", function($scope, $http, $location, $rootScope, hotkeys, $state, $templateCache){
+        $rootScope.go = function ( path ) {
+            $location.path( path );
+        };
+
+        hotkeys.add({
+            combo: 'n',
+            description: 'create a new builder',
+            callback: function(){
+                $rootScope.go("/new-builder");
+            }
+        });
+        hotkeys.add({
+            combo: 'f',
+            description: 'fullscreen',
+            callback: function(){
+                $rootScope.go("/fullscreen");
+            }
+        });
+        hotkeys.add({
+            combo: 'p',
+            description: 'preferences',
+            callback: function(){
+                $rootScope.go("/preferences");
+            }
+        });
+        hotkeys.add({
+            combo: 'f',
+            description: 'dashboard',
+            callback: function(){
+                $rootScope.go("/");
+            }
+        });
+
+        hotkeys.add({
+            combo: 'esc',
+            description: 'previous screen',
+            callback: function(){
+                if ($rootScope.stateStack.length > 0) {
+                    var name = $rootScope.stateStack.pop();
+                    $rootScope.go(name);
+                    $rootScope.stateStack.pop();
+
+                } else {
+                    $rootScope.go("index");
+                }
+                console.log("state stack", $rootScope.stateStack);
+            }
+        });
+        $rootScope.previousState;
+        $rootScope.currentState;
+        $rootScope.stateStack = [];
+        $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
+            if ($rootScope.previousState !== from.name) {
+                $rootScope.previousState = from.name || "/";
+                $rootScope.currentState = to.name || "/";
+            }
+            $rootScope.stateStack.push($rootScope.currentState);
             console.log("state stack", $rootScope.stateStack);
-        }
-    });
-    $rootScope.previousState;
-    $rootScope.currentState;
-    $rootScope.stateStack = [];
-    $rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
-        if ($rootScope.previousState !== from.name) {
-            $rootScope.previousState = from.name || "/";
-            $rootScope.currentState = to.name || "/";
-        }
-        $rootScope.stateStack.push($rootScope.currentState);
-        console.log("state stack", $rootScope.stateStack);
-    });
-    $rootScope.$state = $state;
-    $rootScope.$on("$viewContentLoaded", function() {
-        $templateCache.removeAll();
-    });
-    $('pre code').each(function(i, block) {
-        hljs.highlightBlock(block);
-    });
+        });
+        $rootScope.$state = $state;
+        $rootScope.$on("$viewContentLoaded", function() {
+            $templateCache.removeAll();
+        });
+        $('pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
 
 
-});
+    });
