a1d1bac9995bee192613e0074ca0f24f5c54383f
Gabriel Falcão <gabriel@nacaolivre.org>
2015-06-01 23:14:37 -0400

    cleanup

--- tests/functional/helpers.py
+++ tests/functional/helpers.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # flake8: noqa
-
+import inspect
 import logging
 from sure import scenario
 
 from tumbler.core import Web
-from jaci.models import Builder, JaciPreference, Build
+import jaci.models
 from cqlengine import connection
+from cqlengine.models import Model
 from cqlengine.management import sync_table, drop_table, create_keyspace
 from jaci.api.v1 import web
+
+
+def is_model(v):
+    return (
+        isinstance(v, type) and
+        issubclass(v, Model) and
+        v != Model
+    )
+
+def get_models():
+    return [v for (k, v) in inspect.getmembers(jaci.models) if is_model(v)]
 
 
 def prepare_db(context):
@@ -18,8 +30,8 @@ def prepare_db(context):
     #                { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
     context.connection = connection.setup(['127.0.0.1'], 'jaci')
     create_keyspace('jaci', strategy_class='SimpleStrategy', replication_factor=3, durable_writes=True)
-    tables = [Builder, Build, JaciPreference]
-    for t in tables:
+
+    for t in get_models():
         try:
             drop_table(t)
             sync_table(t)
@@ -36,9 +48,8 @@ def prepare_http_client(context):
 
 
 def clean_db(context):
-    sync_table(Builder)
-    sync_table(Build)
-    sync_table(JaciPreference)
+    for t in get_models():
+        sync_table(t)
 
 safe_db = scenario(prepare_db, clean_db)
 api = scenario([prepare_db, prepare_http_client], [clean_db])
