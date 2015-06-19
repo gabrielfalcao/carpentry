9c1ec946671622f1ef6e6856b847d6d8537f65c6
Gabriel Falcão <gabriel@nacaolivre.org>
2015-06-19 01:34:12 -0400

    adding dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@
 
 from setuptools import setup, find_packages
 
+requirements = [
+    'tumbler==0.0.19',
+    'cqlengine==0.21.0',
+    'blist==1.3.6',
+    'bcrypt==1.1.1',
+    'ansiconv==1.0.0',
+    'gunicorn==19.3.0',
+    'ansi2html==1.1.0',
+    'lineup==0.1.4',
+    'python-dateutil==2.4.2',
+]
+
 setup(
     name='jaci',
     version='0.0.0',
@@ -16,5 +28,6 @@ setup(
     url='http://falcao.it/jaci',
     packages=find_packages(exclude=['*tests*']),
     include_package_data=True,
+    install_requires=requirements,
     zip_safe=False,
 )
