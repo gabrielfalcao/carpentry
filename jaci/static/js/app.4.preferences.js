b4e72f2358a156424cf06b7197bc43fff7d16843
Gabriel Falcão <gabriel@nacaolivre.org>
2015-06-19 11:17:35 -0400

    saving preferences. closes #8

--- jaci/static/js/app.4.preferences.js
+++
@@ -1,2 +0,0 @@
-angular.module('JaciApp.Preferences', ['JaciApp.Common']).controller('PreferencesController', function ($rootScope, $scope, $state, $http, $cookies, hotkeys, notify) {
-});
---
+++ jaci/static/js/app.5.preferences.js
@@ -0,0 +1,11 @@
+angular.module('JaciApp.Preferences', ['JaciApp.Common']).controller('PreferencesController', function ($rootScope, $scope, $state, $http, $cookies, hotkeys, notify) {
+    $scope.savePreferences = function(data){
+        $http.post('/api/preferences', data).
+            success(function(data, status, headers, config) {
+                console.log("/api/preferences", data)
+            }).
+            error(function(data, status, headers, config) {
+                console.log("/api/preferences", data)
+            });
+    };
+});
