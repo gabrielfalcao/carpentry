34aa58b53d6b2a7d055a4a0a3e5b28e4eaa7457b
Gabriel Falcão <gabriel@nacaolivre.org>
2015-06-21 00:47:46 -0400

    0.0.13

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ requirements = [
 
 setup(
     name='jaci',
-    version='0.0.12',
+    version='0.0.13',
     description='continuous integration for the people',
     entry_points={
         'console_scripts': ['jaci = jaci.cli:main'],
