84d38457bc414815647b338787ee05987bfe28be
Gabriel Falcão <gabriel@nacaolivre.org>
2015-06-25 21:30:33 -0400

    Release 0.1.9

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ requirements = [
     'GitHub-Flask>=2.0.1',
     'lineup>=0.1.7',
     'docker-py>=1.2.3',
-    'milieu>=0.1.8',
+    'milieu>=0.1.9',
     'plant>=0.1.2',
     'pycrypto>=2.6',
     'pyOpenSSL>=0.15.1',
@@ -53,7 +53,7 @@ requirements = [
 
 setup(
     name='carpentry-ci',
-    version='0.1.8',
+    version='0.1.9',
     description='continuous integration for the people',
     entry_points={
         'console_scripts': ['carpentry = carpentry.cli:main'],
