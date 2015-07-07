ec1eacc9e424e70cfff9d2d72b1df52f1d5e2b49
Gabriel Falcão <gabriel@nacaolivre.org>
2015-07-07 00:56:10 -0400

    Release 0.1.44

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ requirements = [
 
 setup(
     name='carpentry-ci',
-    version='0.1.43',
+    version='0.1.44',
     description='continuous integration for the people',
     entry_points={
         'console_scripts': ['carpentry = carpentry.cli:main'],
