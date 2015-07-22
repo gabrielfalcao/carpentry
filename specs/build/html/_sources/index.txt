703882e7f5a1dbc4951781190de603affde8700b
Gabriel Falcão <gabriel@nacaolivre.org>
2015-07-21 22:50:25 -0400

    docs

---
+++ specs/build/html/_sources/index.txt
@@ -0,0 +1,25 @@
+.. Carpentry CI documentation master file, created by
+   sphinx-quickstart on Tue Jul 21 18:41:16 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+######################################
+Continuous integration for the people!
+######################################
+
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   overview
+   quickstart
+   usage
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- specs/source/conf.py
+++ specs/source/conf.py
@@ -115,14 +115,16 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
